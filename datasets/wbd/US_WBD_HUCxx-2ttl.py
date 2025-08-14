5dfe3c5fd51b79170c103eb9a7731fc817d6975b
David Kedrowski <96209091+dkedrowski@users.noreply.github.com>
2025-08-14 12:53:38 -0400

    Update US_WBD_HUCxx-2ttl.py

--- datasets/wbd/US_WBD_HUCxx-2ttl.py
+++ datasets/wbd/US_WBD_HUCxx-2ttl.py
@@ -5,11 +5,9 @@ Setup instructions
 
 Required packages:
 * geopandas
-* shapely
-* pandas
 * zip
 * rdflib (Graph and Literal)
-* rdflib.namespace (GEO, OWL, PROV, RDF, RDFS, SDO, and XSD)
+* rdflib.namespace (GEO, RDF, SDO, and XSD)
 * pathlib (Path)
 * namespaces (a local .py file with a dictionary of project namespaces)
 
@@ -18,11 +16,9 @@ Functions:
 """
 
 import geopandas as gpd
-import shapely
-import pandas as pd
 from pathlib import Path
 from rdflib import Graph, Literal
-from rdflib.namespace import GEO, OWL, PROV, RDF, RDFS, SDO, XSD
+from rdflib.namespace import GEO, RDF, SDO, XSD
 
 import logging
 import time
@@ -69,14 +65,15 @@ logging.basicConfig(filename=logname,
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 logger.info('')
 logger.info('LOGGER INITIALIZED')
 
 
-def load_huc_layer(gpkg_uri: str, level: int) -> gpd.GeoDataFrame:
+def load_huc_layer(gpkg_uri: str, level: int):
     try:
         gdf = gpd.read_file(gpkg_uri, layer=f'WBDHU{level}')
-        print(f'Level {level} has {gdf.shape[0]} rows')
+        # print(f'Level {level} has {gdf.shape[0]} rows (from {gpkg_uri}).')
         return gdf
     except Exception as e:
         print(f'Error loading layer: {e}')
@@ -94,34 +91,53 @@ def initial_kg(_PREFIX):
     return graph
 
 
-def build_iris():
-    pass
+def build_iris(hid, level, _PREFIX):
+    huc_iri = f'd.HUC{level}.' + str(hid)
+    return _PREFIX["wbd_data"][huc_iri], _PREFIX["wbd_data"][huc_iri + '.geometry']
 
 
-def process_huc_level_2_to_8(gpkg, huclevel):
-    gdf = load_huc_layer(gpkg, huclevel)
+def process_huc(gpkg, level, graph, _PREFIX):
+    logger.info(f'Load HUC{level} boundary file from {gpkg}.')
+    gdf = load_huc_layer(gpkg, level)
+    gdf.rename(columns={f'huc{level}': 'huc_num'}, inplace=True)
+    logger.info(f'Triplify HUC{level} boundary data.')
+    for row in gdf.itertuples():
+        huciri, geomiri = build_iris(row.huc_num, level, _PREFIX)
+        if ' ' not in huciri:
+            graph.add((huciri, RDF.type, _PREFIX["wbd"][f'HUC{level}']))
+            graph.add((huciri, _PREFIX["wbd"]['hucCode'], Literal(row.huc_num, datatype=XSD.string)))
+            graph.add((huciri, SDO.name, Literal(row.name, datatype=XSD.string)))
+            graph.add((huciri, GEO.hasGeometry, geomiri))
+            graph.add((huciri, GEO.defaultGeometry, geomiri))
+            graph.add((geomiri, RDF.type, GEO.Geometry))
+            graph.add((geomiri, GEO.asWKT, Literal(row.geometry, datatype=GEO.wktLiteral)))
+            for state in [ st.strip() for st in row.states.split(',') ]:
+                graph.add((huciri, _PREFIX["wbd"]['hucState'], Literal(state, datatype=XSD.string)))
+                # This could also be done as an object property linking to AR1 instances from the Spatial repo
+            if level > 8:  # That is, HUCs 10 or 12 only
+                graph.add((huciri, _PREFIX["wbd"]['hucType'], Literal(row.hutype, datatype=XSD.string)))
+                graph.add((huciri, _PREFIX["wbd"]['hucTypeDescription'], Literal(row.hutype_description, datatype=XSD.string)))
+            if level > 10 and row.tohuc.lower() != 'closed basin':  # That is, HUC 12 only
+                    tohuciri, tohucgeomiri = build_iris(row.tohuc, level, _PREFIX)
+                    graph.add((huciri, _PREFIX["wbd"]['toHUC'], tohuciri))
+            if level > 2:
+                containing_huciri, containing_hucgeomiri = build_iris(row.huc_num[:-2], level - 2, _PREFIX)
+                graph.add((huciri, _PREFIX["wbd"]['containingHUC'], containing_huciri))
+    return graph
 
 
-def process_huc_level_10(gpkg):
-    gdf = load_huc_layer(gpkg, '10')
-
-
-def process_huc_level_12(gpkg):
-    gdf = load_huc_layer(gpkg, '12')
-
-
-def write_data_to_ttl():
-    pass
+def write_graph_to_ttl(graph, outfile):
+    logger.info(f'Serialize the triples to {outfile}')
+    graph.serialize(outfile, format='ttl')
 
 
 if __name__ == '__main__':
     start_time = time.time()
     logger.info(f'Launching script: VPU = {vpunum}')
     gpkg_handle = f'/vsizip/{wbd_file}/{gpkg_name}'
-    for level in [2, 4, 6, 8]:
-        process_huc_level_2_to_8(gpkg_handle, level)
-    process_huc_level_10(gpkg_handle)
-    process_huc_level_12(gpkg_handle)
-    write_data_to_ttl()
+    kg = initial_kg(_PREFIX)
+    for huclevel in [2, 4, 6, 8, 10, 12]: # Only triplify up to level 12 as it is the last conterminous level
+        kg = process_huc(gpkg_handle, huclevel, kg, _PREFIX)
+    write_graph_to_ttl(kg, main_ttl_file)
     logger.info(f'Runtime: {str(datetime.timedelta(seconds=time.time() - start_time))} HMS')
     print(f'\nRuntime: {str(datetime.timedelta(seconds=time.time() - start_time))} HMS')
