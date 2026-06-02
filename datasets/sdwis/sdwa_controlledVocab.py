160907e1099029435f88a46582d65083e097c66b
David Kedrowski <96209091+dkedrowski@users.noreply.github.com>
2026-06-02 18:59:00 -0400

    Add provenance

--- datasets/sdwis/sdwa_controlledVocab.py
+++ datasets/sdwis/sdwa_controlledVocab.py
@@ -7,10 +7,6 @@ import logging
 import sys
 from pathlib import Path
 
-## declare variables
-state = "IL"
-controlledVocab = True
-
 ## data paths
 cwd = Path(__file__).resolve().parent
 ns_dir = cwd.parent.parent.parent
@@ -24,7 +20,7 @@ log_dir = cwd / "logs"
 # print(f"Logging directory:              {log_dir}")
 
 ## namespaces
-
+ontologyIRI = URIRef('http://sawgraph.spatialai.org/v1/us-sdwis')
 prefixes = {}
 prefixes['us_sdwis'] = Namespace(f'http://sawgraph.spatialai.org/v1/us-sdwis#')
 prefixes['us_sdwis_data'] = Namespace(f'http://sawgraph.spatialai.org/v1/us-sdwis-data#')
@@ -98,6 +94,7 @@ def triplify(codes):
         kg2.add((iris['type'], RDF.type, prefixes['us_sdwis']['PWS-SubFeatureType']))
         kg2.add((iris['type'], RDF.type, OWL.NamedIndividual))
         kg2.add((iris['type'], RDFS.label, Literal(row['VALUE_DESCRIPTION'])))
+        kg2.add((iris['type'], RDFS.isDefinedBy, ontologyIRI))
     # SubFeature Activity controlled vocabulary
     activity_type = codes[codes['VALUE_TYPE'] == 'ACTIVITY_CODE']
     for idx, row in activity_type.iterrows():
@@ -107,6 +104,7 @@ def triplify(codes):
         kg2.add((iris['activity'], RDF.type, prefixes['us_sdwis']['PWS-SubFeatureActivity']))
         kg2.add((iris['activity'], RDF.type, OWL.NamedIndividual))
         kg2.add((iris['activity'], RDFS.label, Literal(row['VALUE_DESCRIPTION'])))
+        kg2.add((iris['activity'], RDFS.isDefinedBy, ontologyIRI))
     # Service Area Type controlled vocabulary
     serviceAreaType = codes[codes['VALUE_TYPE'] == 'SERVICE_AREA_TYPE_CODE']
     for idx, row in serviceAreaType.iterrows():
@@ -116,6 +114,7 @@ def triplify(codes):
         kg2.add((iris['serviceAreaType'], RDF.type, prefixes['us_sdwis']['PWS-ServiceAreaType']))
         kg2.add((iris['serviceAreaType'], RDF.type, OWL.NamedIndividual))
         kg2.add((iris['serviceAreaType'], RDFS.label, Literal(row['VALUE_DESCRIPTION'])))
+        kg2.add((iris['serviceAreaType'], RDFS.isDefinedBy, ontologyIRI))
     return kg2
 
 
